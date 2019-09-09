f0c821ebf15ddaededc2fdaae268af7963ec30be
Alexei Accio <alexei.accio@gmail.com>
2019-09-09 09:05:24 +0300

    Update readSheet.js

--- readSheet.js
+++ readSheet.js
@@ -1,71 +1,40 @@
-const fs = require("fs");
-const readline = require("readline");
 const { google } = require("googleapis");
+const privatekey = require("./krapiva-0734748d4bd3.json");
 
-const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
-const TOKEN_PATH = "token.json";
 const SHEET_ID = "1dLj99jB8EsdemDuK2LQ0hV580fOfyqIfdsjb3q0S-yo";
 
-function authorize(credentials, callback) {
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris[0]
-  );
+let jwtClient = new google.auth.JWT(
+  privatekey.client_email,
+  null,
+  privatekey.private_key,
+  ["https://www.googleapis.com/auth/spreadsheets"]
+);
 
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
+jwtClient.authorize(function(err, tokens) {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log("Successfully connected!");
+  }
+});
 
-function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: SCOPES
-  });
-  console.log("Authorize this app by visiting this url:", authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question("Enter the code from that page here: ", code => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return callback(err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
-        if (err) console.error(err);
-        console.log("Token stored to", TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
+async function readSheet(range, cb) {
+  const sheets = google.sheets("v4");
 
-const readSheet = (range, cb) =>
-  fs.readFile("credentials.json", (err, content) => {
-    if (err) return console.log("Error loading client secret file:", err);
-    authorize(JSON.parse(content), auth => readRange(auth, range, cb));
-  });
-
-function readRange(auth, range, cb) {
-  const sheets = google.sheets({ version: "v4", auth });
-  sheets.spreadsheets.values.get(
+  return sheets.spreadsheets.values.get(
     {
+      auth: jwtClient,
       spreadsheetId: SHEET_ID,
       range: range,
       majorDimension: "ROWS",
       valueRenderOption: "FORMATTED_VALUE"
     },
-    function(err, result) {
+    function(err, response) {
       if (err) {
-        console.log(err);
+        console.log("The API returned an error: " + err);
       } else {
-        cb(result.data.values);
+        cb(response.data.values);
       }
     }
   );
